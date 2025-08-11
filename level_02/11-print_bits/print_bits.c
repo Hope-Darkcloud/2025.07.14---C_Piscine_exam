5f04762601f7400cdd84e5bffcad03d364df9117
Umit Karabulut <umitkarabulut1989@icloud.com>
2025-08-11 02:49:44 +0200

    Préparation Pour Piscine (PPP)

---
+++ level_02/11-print_bits/print_bits.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+void	print_bits(unsigned char octet){
+    int i = 7;
+    while(i >= 0){
+        if ((octet >> i)&1)
+            write(1, "1", 1);
+        else write(1, "0", 1);
+        i--;
+    }
+    
+    
+}
+
+int main(void)
+{
+    unsigned char n = 2; // Örnek değer
+    print_bits(n);
+    write(1, "\n", 1); // Yeni satır için
+    return 0;
+}
+ 
+
+
+
+
